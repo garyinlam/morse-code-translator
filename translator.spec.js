00d8f978d621896eb8dd41ffc6c867d3b1c95701
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-01 16:21:02 +0100

    letter tests

--- translator.spec.js
+++ translator.spec.js
@@ -1,7 +1,9 @@
 import { translateToMorse } from "./translator";
 
+//klmnopqrstuvwxyz
+
 describe('Testing translateToMorse', () => {
-  it("'a' should translate to be .-", () => {
+  it("'a' should translate to be '.-'", () => {
     // Arrange
     let str;
     // Act
@@ -9,5 +11,49 @@ describe('Testing translateToMorse', () => {
     let result = translateToMorse(str);
     // Assert
     expect(result).toBe(".-");
+  });
+
+  it("'bcd' should translate to be '-... -.-. -..'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "bcd"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("-... -.-. -..");
+  });
+
+  it("'e fg hi' should translate to be '. / ..-. --. / .... ..'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "e fg hi"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe(". / ..-. --. / .... ..");
+  });
+
+  it("'J' and 'j' should both translate to be '.---'", () => {
+    // Arrange
+    let str;
+    let strCap;
+    // Act
+    str = "j"
+    strCap = str.toUpperCase();
+    let result = translateToMorse(str);
+    let resultCapital = translateToMorse(strCap);
+    // Assert
+    expect(result).toBe(".---");
+    expect(resultCapital).toBe(".---");
+  });
+
+  it("'klmnopqrstuvwxyz' should translate to be '-.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "klmnopqrstuvwxyz"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("-.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..");
   });
 });
