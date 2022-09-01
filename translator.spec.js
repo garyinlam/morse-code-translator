d0c94f08d0142cc36daf27f4bc50cbf1631e05c8
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-01 16:27:33 +0100

    number tests

--- translator.spec.js
+++ translator.spec.js
@@ -96,4 +96,105 @@ describe('Testing translateToMorse', () => {
     // Assert
     expect(result).toBe("-....-");
   });
+
+  it("'1' should translate to be '.----'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "1"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe(".----");
+  });
+  
+  it("'2' should translate to be '..---'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "2"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("..---");
+  });
+
+  it("'3' should translate to be '...--'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "3"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("...--");
+  });
+
+  it("'4' should translate to be '....-'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "4"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("....-");
+  });
+
+  it("'5' should translate to be '.....'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "5"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe(".....");
+  });
+
+  it("'6' should translate to be '-....'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "6"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("-....");
+  });
+
+  it("'7' should translate to be '--...'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "7"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("--...");
+  });
+
+  it("'8' should translate to be '---..'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "8"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("---..");
+  });
+
+  it("'9' should translate to be '----.'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "9"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("----.");
+  });
+
+  it("'0' should translate to be '-----'", () => {
+    // Arrange
+    let str;
+    // Act
+    str = "0"
+    let result = translateToMorse(str);
+    // Assert
+    expect(result).toBe("-----");
+  });
+
 });
