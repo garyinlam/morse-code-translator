039ced520c431d2ece15c0cf0fc6d69d06e8c34f
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-02 10:44:54 +0100

    working with basic styling

---
+++ dist/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _translator = require("./translator.js");
+
+var inputBox = document.getElementById("inputBox");
+var toMorseButton = document.getElementById("toMorse");
+var output = document.querySelector(".output");
+
+var handleToMorse = function handleToMorse(e) {
+  e.preventDefault();
+  output.innerHTML = (0, _translator.translateToMorse)(inputBox.value);
+};
+
+toMorseButton.addEventListener("click", handleToMorse);
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-import { translateToMorse } from "./translator";
+import { translateToMorse } from "./translator.js";
+
+const inputBox = document.getElementById("inputBox");
+const toMorseButton = document.getElementById("toMorse");
+const output = document.querySelector(".output");
+
+const handleToMorse = (e) => {
+  e.preventDefault();
+  output.innerHTML = translateToMorse(inputBox.value);
+}
+
+toMorseButton.addEventListener("click", handleToMorse)
